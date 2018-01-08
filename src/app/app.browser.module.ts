35bf3f583a139a9ed30983d23ea4f7e9ac0c13a4
Ed Opare-Aryee <ed@thisissoon.com>
2018-01-08 23:47:56 +0000

    refactor(imports): configure paths in tsconfig for easier imports

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 
-import { AppModule } from './app.module';
-import { AppComponent } from './app.component';
+import { AppModule } from '@app/app.module';
+import { AppComponent } from '@app/app.component';
 
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { environment } from '../environments/environment';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { AppComponent } from '@app/app.component';
+import { environment } from '@app/environment';
 
 @NgModule({
   declarations: [
