08a9cd8ce3e05520c14906adf9eb751c96a854a1
Ed Opare-Aryee <ed@thisissoon.com>
2018-06-02 07:02:12 +0100

    fix(universal): updated npm script and angular config to run server side rendered app

--- server.ts
+++ server.ts
@@ -17,17 +17,16 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 const distFolder = join(process.cwd(), 'dist');
+const browserFolder = join(distFolder, 'browser');
 
 // Our index.html we'll use as our template
-const template = readFileSync(
-  join(distFolder, 'browser', 'index.html')
-).toString();
+const template = readFileSync(join(browserFolder, 'index.html')).toString();
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
 const {
   AppServerModuleNgFactory,
   LAZY_MODULE_MAP
-} = require('./dist/server/main.bundle');
+} = require('./dist/server/main');
 
 const {
   provideModuleMap
@@ -41,6 +40,12 @@ app.engine(
   })
 );
 
+const ngApp = (req, res) => {
+  res.render(join(browserFolder, 'index.html'), { req, res });
+};
+
+const routes = [];
+
 app.set('view engine', 'html');
 app.set('views', join(distFolder, 'browser'));
 
@@ -48,8 +53,10 @@ app.set('views', join(distFolder, 'browser'));
 app.get('*.*', express.static(join(distFolder, 'browser')));
 
 // All regular routes use the Universal engine
-app.get('*', (req, res) => {
-  res.render(join(distFolder, 'browser', 'index.html'), { req });
+app.get('/', ngApp);
+routes.forEach(route => {
+  app.get(`/${route}`, ngApp);
+  app.get(`/${route}/*`, ngApp);
 });
 
 // Start up the Node server
